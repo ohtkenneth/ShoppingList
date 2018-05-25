221749873131eedd8bca99cf405071aa75895038
Kenneth <ohtkenneth@gmail.com>
2018-05-25 14:30:04 -0700

    Added core components

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'shopping-list-component',
+	templateUrl: './shopping-list.component.html',
+	styleUrls: [ './shopping-list.component.css ']
+})
+
+export class ShoppingList {
+	title: 'Shopping list';
+}
